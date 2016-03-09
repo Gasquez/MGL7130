2c3f804cbdd880b34533905cf7d6e952b212d7ec
tgasquez <tom_alias_ghal@hotmail.fr>
2016-03-10 00:15:05 +0100

    suppression des markers pour tout faire via les evenements

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,8 @@ var angularApp = angular.module('MenuNav', ['ionic']);
 var evenement1 = new Object();
  evenement1.titre ="Old help cause for student";
  evenement1.ID = "236554";
- evenement1.latlng = "45.514887, -73.559727";
+ evenement1.lat = "45.514887";
+ evenement1.lng = "-73.559727";
  evenement1.organisme ="UQAM";
  evenement1.siege ="adresse du siege";
  evenement1.webSite="uqam.ca";
@@ -28,7 +29,8 @@ var evenement1 = new Object();
 var evenement2 = new Object();
  evenement2.titre ="Other event";
  evenement2.ID = "236554";
- evenement2.latlng = "45.514887, -73.559727";
+ evenement2.lat = "45.52243499999999"
+ evenement2.lng = "-73.60211099999998";
  evenement2.organisme ="UQAM";
  evenement2.siege ="adresse du siege";
  evenement2.webSite="uqam.ca";
@@ -156,21 +158,17 @@ angularApp.controller("HomeCtrl", function($scope, $ionicNavBarDelegate){
 		};
 
 		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-		var markers = [
-			['Old help, 23/02/16 10:00 PM', 45.514887, -73.559727, 4, evenement1],
-			['Other Event, 00/00/00 00:00 PM', 45.52243499999999, -73.60211099999998,4,evenement2],
-		];
 
-		// Info windows displayed above each markers
-		var infowindow = new google.maps.InfoWindow();
+		// Loop through the array of evenements and place each one on the map 
+		for(i = 0; i < evenementsData.length; i += 1) {
 
-		// Loop through the array of markers and place each one on the map 
-		for(i = 0; i < markers.length; i += 1) {
-		
+			//Loading information events from bdd
+			var currentEvenement = evenementsData[i];
+
 			var marker = new MarkerWithLabel({
-					position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+					position: new google.maps.LatLng(currentEvenement.lat,currentEvenement.lng),
 					map: map,
-					labelContent: "21-02",
+					labelContent: currentEvenement.date,
 					labelAnchor: new google.maps.Point(13, 10),
 				    labelClass: "labels", // the CSS class for the label
 				    labelInBackground: false,
@@ -185,35 +183,26 @@ angularApp.controller("HomeCtrl", function($scope, $ionicNavBarDelegate){
 		
 
 			// Add click action on each marcker
-			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			google.maps.event.addListener(marker, 'click', (function(currentEvenement) {
 			  	return function() {
-			  		infowindow.setContent(markers[i][0]);
-			  		infowindow.open(map, marker);
-
 			      // Display event informations
-			      eventInfoContent = markers[i][1] + " - " + markers[i][2];
 			      angularScope.$apply(function() {
-			      	currentEvenement = markers[i][4];
-			      	angularScope.eventSelected = { 
+			      	angularScope.eventSelected = {
 			      		name: currentEvenement.titre,
-						afterName: ', ' + currentEvenement.date + ', ' + currentEvenement.heure,
+						afterName: ', ' +currentEvenement.date + ', ' + currentEvenement.heure,
 			      		desc: '<b> Descriptif : </b>' + currentEvenement.descriptif + 
 			      		'<br />' + 
 			      		'<b>Activités : </b>'+ currentEvenement.activity
 			      	};
 			      });
 				}
-			})(marker, i));
+			})(currentEvenement));
 		}
 
 		if (navigator.geolocation)
 		  var watchId = navigator.geolocation.watchPosition(function(position){
 		  	//Move map to position
 		  	map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-//For test :var marker = new google.maps.Marker({
-//			    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude), 
-//			    map: map
-//		 	}); 
 		  }, null, {enableHighAccuracy:true});
 		WidthChange(window.matchMedia("(min-width: 640px)"));
 
