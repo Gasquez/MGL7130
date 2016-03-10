0b7c4cff7623845f0f0767ff6ded021e78aa0fa6
Warlot-PQ <warlot.pierre.quentin@gmail.com>
2016-03-11 05:38:27 +0800

    ajout interface evenement selectionné depuis l'interface Home

--- www/js/index.js
+++ www/js/index.js
@@ -1,60 +1,24 @@
 angular.module('starter', ['ionic', 'ngCordova']);
 var angularApp = angular.module('MenuNav', ['ionic']);
 
-/* For test, waiting for JSON data */
-var evenement1 = new Object();
- evenement1.titre ="Old help cause for student";
- evenement1.ID = "236554";
- evenement1.lat = "45.514887";
- evenement1.lng = "-73.559727";
- evenement1.organisme ="UQAM";
- evenement1.siege ="adresse du siege";
- evenement1.webSite="uqam.ca";
- evenement1.emplacement = "at the place";
- evenement1.date ="23/02/2016";
- evenement1.heure ="10h00";
- evenement1.langue ="france";
- evenement1.causes = "Old Help";
- evenement1.activity= "aide et accueil";
- evenement1.avantages = "contact avec les gens";
- evenement1.periode ="one time";
- evenement1.periodicity = "mensuel";
- evenement1.contact = "contact@UQAM.ca";
- evenement1.acces = "Metro place des arts";
- evenement1.descriptif ="Un evenement1 pour aider les personnes agee a faire les papiers ainsi que leurs courses bla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla bla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla bla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla";
- evenement1.accessibleToDisabled = true;
- evenement1.inscred = false;
- evenement1.favored = false;
+var json = (function() {
+	json = null;
+	$.ajax({
+	    'async': false,
+	    'global': false,
+	    'url': "/data.json",
+	    'dataType': "json",
+	    'success': function (data) {
+	        json = data;
+	    }
+	});
+	return json;
+})();
 
-var evenement2 = new Object();
- evenement2.titre ="Other event";
- evenement2.ID = "236554";
- evenement2.lat = "45.52243499999999"
- evenement2.lng = "-73.60211099999998";
- evenement2.organisme ="UQAM";
- evenement2.siege ="adresse du siege";
- evenement2.webSite="uqam.ca";
- evenement2.emplacement = "at the place";
- evenement2.date ="23/02/2016";
- evenement2.heure ="10h00";
- evenement2.langue ="france";
- evenement2.causes = "Old Help";
- evenement2.activity= "aide et accueil";
- evenement2.avantages = "contact avec les gens";
- evenement2.periode ="one time";
- evenement2.periodicity = "mensuel";
- evenement2.contact = "contact@UQAM.ca";
- evenement2.acces = "Metro place des arts";
- evenement2.descriptif ="other event descp";
- evenement2.accessibleToDisabled = true;
- evenement2.inscred = false;
- evenement2.favored = false;
-
-var evenementsData = new Array(evenement1,evenement2);
-/* ********************** */
+var evenementsData = json.evenements;
 
 angularApp.factory('BookMarkFactory', function() {
-  return {
+  return { 
     all: function() {
 		var projectString = window.sessionStorage['BookMark'];
 		if(projectString) {
@@ -152,6 +116,13 @@ angularApp.config(function($stateProvider, $urlRouterProvider) {
 		templateUrl: 'filter.html',
 		controller: 'AppCtrl'
 	})
+
+	.state('homeEvent', {
+		url: '/homeEventlter',
+		templateUrl: 'homeEvent.html',
+		controller: 'AppCtrl'
+	})
+
 	.state('list', {
 		url: '/list',
 		templateUrl: 'list.html',
@@ -253,9 +224,9 @@ angularApp.controller("HomeCtrl", function($scope, $ionicNavBarDelegate){
 			var currentEvenement = evenementsData[i];
 
 			var marker = new MarkerWithLabel({
-					position: new google.maps.LatLng(currentEvenement.lat,currentEvenement.lng),
+					position: new google.maps.LatLng(currentEvenement.latitude,currentEvenement.longitude),
 					map: map,
-					labelContent: currentEvenement.date,
+					labelContent: "currentEvenement.date",
 					labelAnchor: new google.maps.Point(13, 10),
 				    labelClass: "labels", // the CSS class for the label
 				    labelInBackground: false,
@@ -276,10 +247,10 @@ angularApp.controller("HomeCtrl", function($scope, $ionicNavBarDelegate){
 			      angularScope.$apply(function() {
 			      	angularScope.eventSelected = {
 			      		name: currentEvenement.titre,
-						afterName: ', ' +currentEvenement.date + ', ' + currentEvenement.heure,
-			      		desc: '<b> Descriptif : </b>' + currentEvenement.descriptif + 
+						afterName: ', ' + "currentEvenement.date" + ', ' + "currentEvenement.heure",
+			      		desc: '<b> Descriptif : </b>' + currentEvenement.descriptifShort + 
 			      		'<br />' + 
-			      		'<b>Activités : </b>'+ currentEvenement.activity
+			      		'<b>Activités : </b>'+ currentEvenement.activities
 			      	};
 			      });
 				}
@@ -315,6 +286,10 @@ angularApp.controller("HomeCtrl", function($scope, $ionicNavBarDelegate){
 
 	};
 	google.maps.event.addDomListener(window, "load", initialize);
+});
+
+angularApp.controller("HomeEventCtrl", function($scope){
+	angularScope.itemSelected = evenementsData[1];
 });
 
 angularApp.controller("FavoriteCtrl", function($scope, BookMarkFactory){
