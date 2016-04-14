b8cf423001c651bf3dd9fd1f00eb74b9961e4b17
tgasquez <tom_alias_ghal@hotmail.fr>
2016-04-15 01:31:44 +0200

    correctif bug mineur

--- www/js/index.js
+++ www/js/index.js
@@ -457,7 +457,9 @@ angularApp.controller("HomeCtrl", function($scope,$http, $ionicNavBarDelegate, B
 
 	angularScope.$parent.$on('$ionicView.afterEnter', function() {
 		applyFilter();
-		markerClusters.redraw();
+		if(markerCluster != null){
+			markerCluster.redraw();
+		}
 	});
 
 	angularScope.changeBookMark = function(eventObj) {
@@ -503,6 +505,366 @@ angularApp.controller("HomeCtrl", function($scope,$http, $ionicNavBarDelegate, B
 
 		angularScope.itemSelected = evenementsData[0];
 		// Loop through the array of evenements and place each one on the map 
+		for(i = 0; i < evenementsData.length; i += 1) {
+
+			//Loading information events from bdd
+			var itemSelected = evenementsData[i];
+			var lab = itemSelected.date[0];
+			var Lat = itemSelected.latitude;
+			var Lgn = itemSelected.longitude;
+
+			var marker = new MarkerWithLabel({
+				position: new google.maps.LatLng(Lat,Lgn),
+				labelContent: lab,
+				labelAnchor: new google.maps.Point(13, 10),
+			    labelClass: "labels", // the CSS class for the label
+			    labelInBackground: false,
+			    labelVisible: true,
+			    isClicked: false,
+				icon: {
+                	path: google.maps.SymbolPath.CIRCLE,
+			        scale: 16,
+			        fillColor: "#FF0000",
+			        fillOpacity: 1,
+			        strokeWeight: 0.8
+			    }					
+			});
+			markers.push(marker);
+			// Add click action on each marcker
+			google.maps.event.addListener(markers[i], 'click', (function(evenementsData,i) {
+			  	return function() {
+			      // Display event informations
+			      angularScope.$apply(function() {
+			      	angularScope.itemSelected = evenementsData[i];
+
+			      	angularScope.itemInFavorite = BookMarkFactory.exist(angularScope.itemSelected);
+			      });
+				}
+			})(evenementsData,i));
+
+			// change color marker on click
+			google.maps.event.addListener(marker, 'click', function(e) {
+				this.isClicked = true;
+				this.set('labelClass', 'labels active');     	       
+		    });
+
+			// reset color marker on double click
+		    google.maps.event.addListener(marker, 'dblclick', function(e) {
+		    	this.isClicked = false;
+		    	this.set('labelClass', 'labels');
+			});
+
+		    // change color marker on mouseover
+		    google.maps.event.addListener(marker, 'mouseover', function(e) {
+		        this.set('labelClass', 'labels hover');
+		    });
+
+		    // change or reset color marker on mouseout
+		    google.maps.event.addListener(marker, 'mouseout', function(e) {
+		        if (this.isClicked){
+		            this.set('labelClass', 'labels active');
+		        } else {
+		            this.set('labelClass', 'labels');
+		        }
+		    });
+		}
+
+	    markerCluster = new MarkerClusterer(map, markers, mcOptions);
+
+		if (navigator.geolocation)
+		  var watchId = navigator.geolocation.watchPosition(function(position){
+		  	//Move map to position
+		  	map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+		  	setPosition(position);
+		  }, null, {enableHighAccuracy:true});
+		handleNavBarVisibility(window.matchMedia("(min-width: 768px)"));
+	};
+
+	// media query event handler
+	if (matchMedia) {
+	  var mq = window.matchMedia("(min-width: 768px)");
+	  mq.addListener(handleNavBarVisibility);
+	  handleNavBarVisibility(mq);
+	}
+
+	// change in fonction of width
+	function handleNavBarVisibility(mq) {
+
+	  if (mq.matches) {
+	  	// if screen >= 768px, hidde nav bar
+	  	 $ionicNavBarDelegate.showBar(false);
+	  } else {
+	  	// if screen < 768px, show nav bar
+	  	$ionicNavBarDelegate.showBar(true);
+	  }
+
+	};
+
+	function applyFilter(){
+		if(markers.length == 0){
+			return;
+		}
+		var valueOfFirstMarker = FilterMarkersService.filterMarkers(markers, markerCluster, map, myPosition);
+		google.maps.event.trigger(map,'resize');
+		if(valueOfFirstMarker != "none"){
+			angularScope.itemSelected =  evenementsData[valueOfFirstMarker];
+		}
+	}
+
+	google.maps.event.addDomListener(window, "load", loadData);
+});
+
+angularApp.controller("HomeEventCtrl", function($scope, $state, $stateParams){
+	var angularScope = $scope;
+	var eventId = $stateParams.eventId;
+
+	if ( $stateParams.hasOwnProperty("eventId") && eventId.trim() != "" ) {
+
+		evenementsData.forEach( function(element, index, array) {
+
+			if ( element.hasOwnProperty("id") && element.id == eventId ) {
+				angularScope.itemSelected = array[index];
+				return;
+			}
+
+		});
+	} else {
+		console.log("Error: no eventId parameter!");
+	}
+});
+
+angularApp.controller("detailEventCtrl", function($scope, UserService) {
+	var angularScope = $scope;
+	angularScope.showLoginView = false;
+	angularScope.participationPending = false;
+
+	angularScope.participateToEvent = function() {
+		// if not logged
+		if (userLogged == false) {
+			angularScope.showLoginView = true;
+			angularScope.participationPending = true;
+		} else {
+			joiningEvent();
+		}
+	}
+
+	angularScope.connectWithFB = function() {
+		// Connect with FB, participate to an event on success if participationPending is true
+
+		facebookConnectPlugin.login(["public_profile"], connectionSucceeded, function(msg){
+			console.log(msg);
+		});
+	}
+
+	angularScope.goBack = function() {
+		angularScope.showLoginView = false;
+	}
+
+	function connectionSucceeded(response) {
+		userLogged = true;
+
+		// Fetch the name
+		facebookConnectPlugin.api("/me", ["public_profile"], function(data){
+			// Fetch the picture
+			facebookConnectPlugin.api("/me/picture?redirect=false", ["public_profile"], function(ret){
+				UserService.setUser(data.name, ret.data.url);
+			}, function(msg){		
+				console.log(msg);
+			});
+		}, function(msg){		
+			console.log(msg);
+		});
+
+		if (angularScope.participationPending == true) {
+			joiningEvent();
+		}
+
+		// Not working, is supposed to hide the connection panel
+		angularScope.goBack();
+	}
+
+	function joiningEvent() {
+		//TODO Register people to the event
+		console.log("participating to an event .....");
+
+
+
+
+	}
+});
+
+angularApp.controller("FavoriteCtrl", function($scope, $window, $state, BookMarkFactory){
+	var angularScope = $scope;
+	angularScope.items = BookMarkFactory.all();
+	angularScope.itemSelected = null;
+	angularScope.itemInFavorite = true; 
+
+	angularScope.changeBookMark = function(eventObj) {
+		// In this controller, items always are in favorite
+		if ( angularScope.itemInFavorite == false ) {
+			// Nothing to do, cannot add event to favorite from here
+		} else {
+			BookMarkFactory.delete(eventObj);
+
+			if (navigator.notification) {
+				navigator.notification.alert( "Favoris supprimé.", null, '', 'Ok' );
+			}
+			else {
+				alert( "Favoris supprimé." );
+			}
+			
+			// Switch to next favorite if exists, otherwise refresh page
+			var favoriteList = BookMarkFactory.all();
+
+			if ( favoriteList.length != 0 ) {
+				angularScope.itemSelected = favoriteList[0];
+			} else {
+				$state.go("home");
+			}
+		}
+	};
+
+	angularScope.masterToDetailMode = function($index) {
+		$('#view').addClass('mode-detail');
+
+		var childNumber = $index + 1; //In angular, $index starts at 0 but starts at 1 with :nth-child 
+		$(".master-item-favorite:nth-child(" + childNumber + ")").addClass('master-item-favorite-selected').siblings().removeClass('master-item-favorite-selected');
+	
+		angularScope.itemSelected = BookMarkFactory.all()[$index];
+	};
+
+	angularScope.detailModeToMaster = function() {
+		$('#view').removeClass('mode-detail');
+	};
+
+	/* For test : open automaticaly first event (we know it exists because data.json is hard-coded)*/
+	if(window.matchMedia("(min-width: 768px)").matches)
+	{
+		angularScope.masterToDetailMode(0);
+	}
+});
+
+angularApp.controller("ListCtrl", function($scope, BookMarkFactory){
+	var angularScope = $scope;
+	angularScope.items = evenementsData;
+	angularScope.itemSelected = evenementsData[0];
+	angularScope.itemInFavorite = false;
+
+	angularScope.changeBookMark = function(eventObj) {
+		if ( angularScope.itemInFavorite == false ) {
+			BookMarkFactory.add(eventObj);
+
+			angularScope.itemInFavorite = true;
+		} else {
+			BookMarkFactory.delete(eventObj);
+
+			angularScope.itemInFavorite = false;
+		}
+	};
+
+	angularScope.masterToDetailMode = function($index) {
+		angularScope.itemInFavorite = false;
+
+		$('#viewList').addClass('mode-detail');
+
+		var childNumber = $index + 1; //In angular, $index starts at 0 but starts at 1 with :nth-child 
+		$(".master-item-list:nth-child(" + childNumber + ")").addClass('master-item-list-selected').siblings().removeClass('master-item-list-selected');
+	
+		angularScope.itemSelected = evenementsData[$index];
+
+		// Check if the new event selected is already inside favorite list
+		if ( BookMarkFactory.exist(angularScope.itemSelected) ) {
+			angularScope.itemInFavorite = true;
+		} else {
+			angularScope.itemInFavorite = false;
+		}
+	};
+
+	angularScope.detailModeToMaster = function() {
+		$('#viewList').removeClass('mode-detail');
+	};
+
+	/* For test : open automaticaly first event (we know it exists because data.json is hard-coded)*/
+	if(window.matchMedia("(min-width: 768px)").matches)
+	{
+		angularScope.masterToDetailMode(0);
+	}
+
+});
+
+angularApp.controller("FilterCtrl", function($scope, PreferencesService){
+	var angularScope = $scope;
+
+
+	angularScope.distance = [
+	    { text: "rayon 1 km", value: "1" },
+	    { text: "rayon 5 km", value: "5" },
+	    { text: "rayon 10 km", value: "10" },
+	    { text: "rayon 30 km", value: "30" },
+	    { text: "rayon > 30 km", value: "++" },
+	    { text: "Aucun", value: "none"}
+	];
+
+	angularScope.temps = [
+	    { text: "< 1 heure", value: "1h00" },
+	    { text: "< 3 heures", value: "3h00" },
+	    { text: "< 1 jour", value: "1j00" },
+	    { text: "> 1 jour", value: "++" },
+	    { text: "Aucun", value: "none"}
+	];
+
+	angularScope.populationCible = [
+		{ text: "Jeune", value: "jeune" },
+	    { text: "Vieille", value: "vieille" },
+	    { text: "Handicapée", value: "handicapee" },
+	    { text: "Aucun", value: "none"}
+	];
+
+  	angularScope.periodicity = [
+	    { text: "Jounalière", value: "journalieres" },
+	    { text: "Quotidienne", value: "quotidienne" },
+	    { text: "Mensuelle", value: "mensuelle" },
+	    { text: "Aucun", value: "none"}
+	];
+
+    angularScope.data = {
+	    distance: PreferencesService.getPreferences('distance'),
+	    temps: PreferencesService.getPreferences('temps'),
+	    populationCible: PreferencesService.getPreferences('populationCible'),
+	    periodicity: PreferencesService.getPreferences('periodicity'),
+ 	};
+
+	angularScope.changePreferences = function(category,item) {
+		if (item.text == "None"){
+			PreferencesService.delete(category);
+		} else {
+			PreferencesService.add(category,item);
+		}
+	};
+});
+
+
+var app = {
+	initialize: function() {
+		this.bindEvents();
+	},
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onDeviceReady: function() {
+		// L'API Cordova est prête
+		console.log("onDeviceReady");
+
+		//Initialize the JS SDK if in browser mode
+		if (window.cordova.platformId == "browser") {
+			facebookConnectPlugin.browserInit('101844923550027', 'v2.5', function(){
+			});			
+		} else {
+			// Nothing to do
+		}
+	}
+};
+app.initialize();
 		for(i = 0; i < evenementsData.length; i += 1) {
 
 			//Loading information events from bdd
